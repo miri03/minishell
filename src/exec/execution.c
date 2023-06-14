3507e47174271e780eda7be9bc6a80335281e28e
miri03 <haritmeryem@gmail.com>
2023-06-14 22:04:17 +0100

    ready to push

--- src/exec/execution.c
+++ src/exec/execution.c
@@ -6,7 +6,7 @@
 /*   By: meharit <meharit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 05:02:22 by meharit           #+#    #+#             */
-/*   Updated: 2023/06/13 17:35:25 by meharit          ###   ########.fr       */
+/*   Updated: 2023/06/14 22:00:53 by meharit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,17 @@
 
 void	execute(t_cmd *table, t_env **dup_env)
 {
-	g_exec.herdoc_pipe = malloc(sizeof(t_exec) * table_len(table));
-	if (!table)
-		return ;
-	if (open_herdoc(table, *dup_env))
-		return ;
-	if (table_len(table) == 1)
-		exec_single(dup_env, table);
-	else
-		multi_cmd(*dup_env, table);
-	free(g_exec.herdoc_pipe);
+	if (table)
+	{
+		g_exec.herdoc_pipe = malloc(sizeof(t_exec) * table_len(table));
+		if (!table)
+			return ;
+		if (open_herdoc(table, *dup_env))
+			return ;
+		if (table_len(table) == 1)
+			exec_single(dup_env, table);
+		else
+			multi_cmd(*dup_env, table);
+		free(g_exec.herdoc_pipe);
+	}
 }
